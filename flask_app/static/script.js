65e8f443be348c1cb5074f7c1520c9d1cb73feac
RochaThomas <tomrocha99@gmail.com>
2023-04-28 21:07:45 -0700

    script removed hover functions

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,74 +1,4 @@
 
-function nav_login_react(element) {
-    element.classList.add("on_hover_navbar_login");
-}
-function nav_login_unreact(element) {
-    element.classList.remove("on_hover_navbar_login");
-}
-
-function nav_link_react(element) {
-    element.classList.add("on_hover_navbar_link");
-}
-function nav_link_unreact(element) {
-    element.classList.remove("on_hover_navbar_link");
-}
-
-function nav_sign_up_react(element) {
-    element.classList.add("on_hover_navbar_sign_up");
-}
-function nav_sign_up_unreact(element) {
-    element.classList.remove("on_hover_navbar_sign_up");
-}
-
-function nav_logout_react(element) {
-    element.classList.add("on_hover_navbar_logout");
-}
-function nav_logout_unreact(element) {
-    element.classList.remove("on_hover_navbar_logout");
-}
-
-function box_react(element) {
-    element.classList.add("on_hover_boxes");
-}
-function box_unreact(element) {
-    element.classList.remove("on_hover_boxes");
-}
-
-function box_button_react(element) {
-    element.classList.add("on_hover_boxes_buttons");
-}
-function box_button_unreact(element) {
-    element.classList.remove("on_hover_boxes_buttons");
-}
-
-function sign_up_react(element) {
-    element.classList.add("on_hover_sign_up_button");
-}
-function sign_up_unreact(element) {
-    element.classList.remove("on_hover_sign_up_button");
-}
-
-function login_react(element) {
-    element.classList.add("on_hover_login_button");
-}
-function login_unreact(element) {
-    element.classList.remove("on_hover_login_button");
-}
-
-function add_react(element) {
-    element.classList.add("on_hover_add_button");
-}
-function add_unreact(element) {
-    element.classList.remove("on_hover_add_button");
-}
-
-function rand_favs_btns_react(element) {
-    element.classList.add("on_hover_red_btn_white_wrd");
-}
-function rand_favs_btns_unreact(element) {
-    element.classList.remove("on_hover_red_btn_white_wrd");
-}
-
 const openIconModal = () => {
     let modal = document.getElementById("info-icon-modal")
     modal.style.display = "block";
@@ -346,8 +276,16 @@ function initMap(){
                     inputContainer.appendChild(placeInput);
                     inputContainer.appendChild(labelPlaceInput);
 
+                    /**
+                     * fix the input containers to no longer show window information
+                     * keep it so that the image changes on the marker
+                     * change to magnifine glass
+                     * make the glass persist until the next glass should appear
+                     * window only appears by interacting with markers
+                     */
+
                     let timer = null;
-                    inputContainer.addEventListener("mouseover", () => {
+                    inputContainer.addEventListener("mouseenter", () => {
                         timer = window.setTimeout(() => {
                             if (!infoWindow.content) {
                                 initInfoWindowContent(place.place_id, price, infoWindow, marker);
